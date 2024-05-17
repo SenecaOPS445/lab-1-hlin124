0cb2e0ea2fd4a36dc026b8282db614be00bb1438
Haley Lin <hlin124@myseneca.ca>
2024-05-17 13:32:56 -0400

    completed lab

---
+++ lab1b.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+name = 'Isaac'
+#print(name)
+#print('name')
+print('How old are you ' + name + '?')
---
+++ lab1c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+name = 'Isaac'
+age = 72
+#print(num1)
+#print(num2)
+#sum = num1 + num2
+#print(sum)
+print(name + ' is ' + str(age) + " years old!")
